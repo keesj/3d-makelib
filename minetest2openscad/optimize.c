1a7291ff9f8cf805233f1f007ae106228b448e49
Kees Jongenburger <kees.jongenburger@gmail.com>
2014-06-16 21:50:44 +0200

    minetest2openscad:ALlow to scale and skip optimize

--- minetest2openscad/optimize.c
+++ minetest2openscad/optimize.c
@@ -8,12 +8,46 @@
 #define ZSIZE 200
 int xyz[XSIZE][YSIZE][ZSIZE];
 
+#include <unistd.h>
+
+void show_usage(){
+	fprintf(stderr,"Usage: optimize [-s] [-g] < in > out.scad\n");
+	fprintf(stderr,"    -s scale up blocks by 10%% to prevent \n");
+	fprintf(stderr,"       edges from touching two squares   \n");
+	fprintf(stderr,"    -g don't optimize                    \n");
+	fprintf(stderr,"    -h show this help                    \n");
+}
 /**
  * This program read the output of the modified minetestmapper found in this same directory
  * minetestmapper.py output a cvs list of lightstone_red_off minetest stones.
  **/
 int main(int argc,char **argv)
 {
+	int scale_up = 0;
+	int do_optimize = 1;
+	char op;
+
+
+	while(( op = getopt(argc, argv, "sgh")) != -1){
+		switch(op){
+		case 's':
+			fprintf(stderr,"scaling up\n");
+			scale_up = 1;
+			break;
+		case 'g':
+			fprintf(stderr,"skipping optimize\n");
+			do_optimize = 0;
+			break;
+		case 'h':
+			show_usage();
+			return 0;
+			break;
+		default:
+			show_usage();
+			return 1;
+			break;
+		}
+	}
 	memset(&xyz,0,sizeof(xyz));
 	 int x,y,z,r;
 	 while ( (r = fscanf(stdin, "%d,%d,%d", &x,&y,&z)) == 3 ) {	
@@ -24,26 +58,27 @@ int main(int argc,char **argv)
 	 }
 	/* now optimize */
 	//printf("translate([%d,%d,%d]) cube([1,1,1]);\n",x,y,z);
-	for (x=0 ; x < XSIZE ; x++){
-		for (y=0 ; y < YSIZE ; y++){
-			for (z=0 ; z < ZSIZE ; z++){
-				if (xyz[x][y][z] == 1){
-					int f,count;
-					count =0;
-					for (f =z ;  f < ZSIZE ; f++){
-						if (xyz[x][y][f] ==1){
-							count++;
-						} else {
-							//printf("making xyz -> %d,%d,%d %d\n",x,y,z,count);
-							xyz[x][y][z] = count;
-							int g;
-							for (g =z+1 ;  g < z + count ; g++){
-								xyz[x][y][g] =0;
+	if (do_optimize) {
+		for (x=0 ; x < XSIZE ; x++){
+			for (y=0 ; y < YSIZE ; y++){
+				for (z=0 ; z < ZSIZE ; z++){
+					if (xyz[x][y][z] == 1){
+						int f,count;
+						count =0;
+						for (f =z ;  f < ZSIZE ; f++){
+							if (xyz[x][y][f] ==1){
+								count++;
+							} else {
+								//printf("making xyz -> %d,%d,%d %d\n",x,y,z,count);
+								xyz[x][y][z] = count;
+								int g;
+								for (g =z+1 ;  g < z + count ; g++){
+									xyz[x][y][g] =0;
+								}
 							}
 						}
 					}
 				}
-			
 			}
 		}
 	}
@@ -52,7 +87,11 @@ int main(int argc,char **argv)
 		for (y=0 ; y < YSIZE ; y++){
 			for (z=0 ; z < ZSIZE ; z++){
 				if (xyz[x][y][z] > 0){
-					printf("translate([%d,%d,%d]) cube([1,1,%d]);\n",x,y,z,xyz[x][y][z]);
+					if (scale_up == 0) {
+						printf("translate([%d,%d,%d]) cube([1,1,%d]);\n",x,y,z,xyz[x][y][z]);
+					} else {
+						printf("translate([%d,%d,%d]) cube([1.1,1.1,%d]);\n",x,y,z,xyz[x][y][z]);
+					}
 				}
 			}
 		}
